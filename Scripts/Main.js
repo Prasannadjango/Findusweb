16afb8a9820d5d42cba220bb207682d2eae23fd8
Prasannadjango <Prasannakumarprasanna1199@gmail.com>
2023-03-14 08:18:04 +0530

    chnages

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -16,3 +16,15 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+  // Popover scripts
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+ var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+  container: 'body'
+})
