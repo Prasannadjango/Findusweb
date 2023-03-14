8dbe707179731af3cfd7b154ab61d50518608ad7
Prasannadjango <Prasannakumarprasanna1199@gmail.com>
2023-03-14 12:12:28 +0530

    chnages

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,30 +1,45 @@
 // Swiper js
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    // spaceBetween: 30,
-    breakpoints: {
-        // when window width is <= 499px
-        
-        320: {
-          slidesPerView: 2,
-          
-          spaceBetween: 10,
-      },
+  slidesPerView: 3,
+  // spaceBetween: 30,
+  breakpoints: {
+    // when window width is <= 499px
+
+    320: {
+      slidesPerView: 2,
+
+      spaceBetween: 10,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    550: {
+      slidesPerView: 3,
+
+      spaceBetween: 20,
     },
-  });
+    768: {
+      slidesPerView: 4,
+
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 4,
+
+      spaceBetween: 30,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 
-  // Popover scripts
+// Popover scripts
 
-  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
 
- var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
   container: 'body'
 })
